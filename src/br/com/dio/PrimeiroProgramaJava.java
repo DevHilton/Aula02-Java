0b3fa92a3ac8e467e39d00a45eb38ad38e2acb8d
shaka1082 <66804236+shaka1082@users.noreply.github.com>
2022-05-14 22:58:19 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+        System.out.println(gato);
+/*
+             int a = 5;
+        int b = 3;
+
+        System.out.println("hello world "+ (a+b));*/
+    }
+
+
+}
